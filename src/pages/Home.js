eadd8e11594a219647a31d5690f12711bccd4ce1
Pharaoh Chirchir <148617667+Chirchirp@users.noreply.github.com>
2025-03-07 20:07:31 +0300

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <section className="home">
+      <h2>Latest Posts</h2>
+      <div className="post-list">
+        <article className="post">
+          <h3><Link to="/blog/1">Understanding Data Cleaning</Link></h3>
+          <img src="/assets/images/blog1.jpg" alt="Data Cleaning" width="300" />
+          <p>A short summary of my first post...</p>
+        </article>
+        <article className="post">
+          <h3><Link to="/blog/2">Exploring SQL for Data Analysis</Link></h3>
+          <img src="/assets/images/blog2.jpg" alt="SQL Techniques" width="300" />
+          <p>A short summary of my second post...</p>
+        </article>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
