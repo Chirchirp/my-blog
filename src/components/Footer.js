8c6aa8a7f23d8c74f2c0a93ab317fb07f8e3b0f3
Pharaoh Chirchir <148617667+Chirchirp@users.noreply.github.com>
2025-03-07 20:06:59 +0300

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Data Analyst Blog</p>
+    </footer>
+  );
+}
+
+export default Footer;
