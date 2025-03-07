87a93944ff8b00166a422a83c8771b5047e56c47
Pharaoh Chirchir <148617667+Chirchirp@users.noreply.github.com>
2025-03-07 20:10:14 +0300

    Create About.js

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <p>I am a data analyst passionate about turning raw data into valuable insights. This blog showcases my journey, projects, and insights into the world of data analysis.</p>
+    </section>
+  );
+}
+
+export default About;
